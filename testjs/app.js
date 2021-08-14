415029954f648d8382d284fbaa3162fe35ae452d
tententgc <nyman3527@gmail.com>
2021-08-14 22:07:15 +0700

    Update app.js

--- testjs/app.js
+++ testjs/app.js
@@ -3,3 +3,14 @@ const cars =new Array("a",'b','c')
 for(let i =0;i<cars.length;i++){
     console.log(cars[i])
 }
+console.log(cars)
+let a =10
+if (a==10){
+    console.log("love ppkkw.")
+}else{
+    console.log("love yourself please")
+}
+
+
+
+
